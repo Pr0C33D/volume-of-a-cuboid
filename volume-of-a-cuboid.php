864af29d34b89c3b484eaa85edbf530e92d70fea
Pr0C33D <hero1393@gmail.com>
2021-10-11 12:46:13 +0300

    DONE

---
+++ volume-of-a-cuboid.php
@@ -0,0 +1,8 @@
+<?php 
+// https://www.codewars.com/kata/58261acb22be6e2ed800003a
+$kata = new class {
+  public function get_volume_of_cuboid($length, $width, $height) {
+    return $length*$width*$height;
+  }
+};
+?>
